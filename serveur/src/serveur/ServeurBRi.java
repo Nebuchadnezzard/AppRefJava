1841ffe2965cd408c55b2a734ed0156db50605c9
Nebuchadnezzard <pierre.couderc.pro@gmail.com>
2018-03-06 17:32:57 +0100

    tentative de progression serveur

--- serveur/src/serveur/ServeurBRi.java
+++ serveur/src/serveur/ServeurBRi.java
@@ -1,20 +1,26 @@
 package serveur;
 
 import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.net.*;
+
+import service.Service;
 
 public class ServeurBRi implements Runnable {
 	private ServerSocket listen_socket;
 	private Class<? extends service.Service> classeService;
+	private Constructor<?> constructeurService;
 
 	// Ajouter un .class en paramètre
-	public ServeurBRi(int port, Class<? extends service.Service> classeService) {
+	public ServeurBRi(int port, Class<? extends service.Service> classeService) throws NoSuchMethodException, SecurityException {
 		try {
 			listen_socket = new ServerSocket(port);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 		this.classeService = classeService;
+		this.constructeurService = classeService.getDeclaredConstructor(java.net.Socket.class);
 	}
 
 	// Le serveur ecoute et accepte les connections.
@@ -23,14 +29,33 @@ public class ServeurBRi implements Runnable {
 	public void run() {
 		try {
 			while(true) {
+			
+				
 				//lancer le service avec la socket en param
+				Socket socketClient = listen_socket.accept();
+				classeService = this.constructeurService.newInstance(socketClient);
+				classeService.start();
 			}
 		}
 		catch (IOException e) { 
 			try {this.listen_socket.close();} catch (IOException e1) {}
 			System.err.println("Pb sur le port d'écoute :"+e);
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
+
+
 
 	// restituer les ressources --> finalize
 	protected void finalize() throws Throwable {
